5a0bc4d20fa30d26709c819bb6ede728fe12b685
tanawat <tanawat@158.108.238.244>
2013-09-11 14:21:01 +0700

    Initial commit
    This is simple number addition program to illustrate the usage 
    Git.

---
+++ Github/src/git/Main.java
@@ -0,0 +1,14 @@
+package git;
+
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Enter A :");
+		int a = scan.nextInt();
+		System.out.println("Enter B : ");
+		int b = scan.nextInt();
+		System.out.println("A + B = "+(a+b));
+	}
+}
