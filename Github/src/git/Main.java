6bcc6c82801ee5d090a2f267da8c837bac462537
tanawat <tanawat@158.108.238.244>
2013-09-11 15:55:42 +0700

    JFrame.

--- Github/src/git/Main.java
+++ Github/src/git/Main.java
@@ -2,9 +2,14 @@ package src.git;
 
 import java.util.Scanner;
 
+import javax.swing.JFrame;
+
 public class Main {
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
+		JFrame frame = new JFrame("This is my User Interface");
+		frame.setSize(400, 400);
+		frame.setVisible(true);
 		System.out.println("Hello!");
 		System.out.println("Enter A :");
 		int a = scan.nextInt();
