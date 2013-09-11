41622c199f1eb156f55a8b6719ac415a139b9c88
tanawat <tanawat@158.108.238.244>
2013-09-11 14:32:57 +0700

    say hello.

--- Github/src/git/Main.java
+++ Github/src/git/Main.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 public class Main {
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
+		System.out.println("Hello");
 		System.out.println("Enter A :");
 		int a = scan.nextInt();
 		System.out.println("Enter B : ");
